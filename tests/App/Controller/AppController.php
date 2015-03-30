c23b89a7840275490877153eaacd413ba54c4a36
BobMulder <bobmulder@outlook.com>
2015-03-30 18:53:47 +0200

    Added newer phpunit version + added test_app

--- tests/App/Controller/AppController.php
+++ tests/App/Controller/AppController.php
@@ -1,10 +1,41 @@
 <?php
-namespace CakeManager\Test\App\Controller;
+/**
+ * CakeManager (http://cakemanager.org)
+ * Copyright (c) http://cakemanager.org
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) http://cakemanager.org
+ * @link          http://cakemanager.org CakeManager Project
+ * @since         1.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
 
 use Cake\Controller\Controller;
 
 class AppController extends Controller
 {
 
-    
+    public function initialize()
+    {
+        $this->loadComponent('Flash');
+
+        $this->loadComponent('CakeManager.Manager');
+        $this->loadComponent('Utils.Authorizer');
+
+        $this->Auth->allow(['display']);
+    }
+
+    public function beforeFilter(\Cake\Event\Event $event)
+    {
+        parent::beforeFilter($event);
+    }
+
+    public function isAuthorized($user)
+    {
+        return false;
+    }
 }
